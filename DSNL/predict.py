419faa0fc218061d31f571016eb3bd93e3544903
Davide-Pedroncelli <84595751+Davide-Pedroncelli@users.noreply.github.com>
2022-10-04 22:19:06 +0200

    Update predict.py

--- DSNL/predict.py
+++ DSNL/predict.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python
-# -*- coding: utf8 -*-
 import os
 import time
 import sys
@@ -25,8 +23,11 @@ from tabulate import tabulate
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
+# Read sys params
 dataset = sys.argv[1]
 model = sys.argv[2]
+
+# Path, dirs
 base_path = "/content/drive/MyDrive/Experiments/DSNL"
 model_dir = f"{base_path}/{dataset}/{model}"
 n_folds = 1
@@ -50,23 +51,7 @@ coding2stages = {
     4: "R"
 }
 
-
-def print_performance(sess, network_name, n_examples, duration, loss, cm, acc, f1):
-    # Get regularization loss
-    reg_loss = tf.add_n(tf.get_collection("losses", scope=network_name + "\/"))
-    reg_loss_value = sess.run(reg_loss)
-
-    # Print performance
-    print((
-        "duration={:.3f} sec, n={}, loss={:.3f} ({:.3f}), acc={:.3f}, "
-        "f1={:.3f}".format(
-            duration, n_examples, loss, reg_loss_value, acc, f1
-        )
-    ))
-    print(cm)
-    print(" ")
-
-
+# Run DSNL
 def run_epoch(
         sess,
         network,
@@ -79,29 +64,31 @@ def run_epoch(
         fold_idx
 
 ):
+    # Useful Variables
     start_time = time.time()
-    y = []
-    y_true = []
-    prob_pred = []
-    ece = []
-    acs = []
-    acc = []
-    mf1 = []
-    wf1 = []
-    k = []
-    f1_w = []
-    f1_n1 = []
-    f1_n2 = []
-    f1_n3 = []
-    f1_r = []
     all_y_true = []
     all_y_pred = []
     all_hypno_true = []
     all_hypno_pred = []
-    
+
+    # Create dict to store performance
+    perf_dict = {
+      "acc" : [],
+      "mf1":[],
+      "k":[],
+      "wf1":[],
+      "f1_w" : [],
+      "f1_n1":[],
+      "f1_n2":[],
+      "f1_n3":[],
+      "f1_r":[],
+      "ece" : [],
+      "acs":[],
+    } 
 
     total_loss, n_batches = 0.0, 0
 
+    # Iterating each subject
     for sub_f_idx, each_data in enumerate(zip(inputs, targets, targets_smoothed)):
         print(".", end="",flush=True)
         each_x, each_y, each_y_cond = each_data
@@ -113,7 +100,7 @@ def run_epoch(
         each_conf_pred = []
 
 
-        # y_batch_seq, batch_len, epochs_shifts
+        # Iterating each subject batch
         for x_batch, y_batch, y_batch_cond in iterate_minibatches_prediction(
                 inputs=each_x,
                 targets=each_y,
@@ -148,51 +135,53 @@ def run_epoch(
             assert not np.isnan(loss_value), \
                 "Model diverged with loss = NaN"
 
-        y.append(each_y_pred)
-        y_true.append(each_y_true)
-        prob_pred.append(each_hypno_pred)
+     
+         # Storing all date to save
+        all_y_true.append(each_y_true)
+        all_y_pred.append(each_y_pred)
+        all_hypno_true.append(each_hypno_true)
+        all_hypno_pred.append(each_hypno_pred)
 
         # Compute ECE
         each_y_true = [int(i) for i in each_y_true]
-        ece.append(compute_calibration(np.array(each_y_true), np.array(each_y_pred), np.array(each_conf_pred), num_bins=20))
+        perf_dict["ece"].append(compute_calibration(np.array(each_y_true), np.array(each_y_pred), np.array(each_conf_pred), num_bins=20))
 
         # Compute ACS
-        acs.append(compute_acs(each_hypno_true, each_hypno_pred))
+        perf_dict["acs"].append(compute_acs(each_hypno_true, each_hypno_pred))
 
         # Compute Performance
-        acc.append(np.mean(accuracy_score(each_y_true, each_y_pred)))
-        mf1.append(f1_score(each_y_true, each_y_pred, average="macro"))
-        k.append(cohen_kappa_score(each_y_true, each_y_pred))
-        wf1.append(f1_score(each_y_true, each_y_pred, average="weighted"))
+        perf_dict["acc"].append(np.mean(accuracy_score(each_y_true, each_y_pred)))
+        perf_dict["mf1"].append(f1_score(each_y_true, each_y_pred, average="macro"))
+        perf_dict["k"].append(cohen_kappa_score(each_y_true, each_y_pred))
+        perf_dict["wf1"].append(f1_score(each_y_true, each_y_pred, average="weighted"))
         f1_ = f1_score(each_y_true, each_y_pred, average=None)
-        cls = np.setdiff1d(np.array([0,1,2,3,4]),np.unique(each_y_pred))
         # Check if there is at least one example for each class
+        cls = np.setdiff1d(np.array([0,1,2,3,4]),np.unique(each_y_pred))
         d = {}
         for n,i in enumerate(np.unique(each_y_pred)):
           d[i] = f1_[n]
         if cls.size > 0:
           for i in cls:
             d[i] = np.nan
-        f1_w.append(d[0])
-        f1_n1.append(d[1])
-        f1_n2.append(d[2])
-        f1_n3.append(d[3])
-        f1_r.append(d[4])
+        perf_dict["f1_w"].append(d[0])
+        perf_dict["f1_n1"].append(d[1])
+        perf_dict["f1_n2"].append(d[2])
+        perf_dict["f1_n3"].append(d[3])
+        perf_dict["f1_r"].append(d[4])
 
-        # Storing all date to save
-        all_y_true.append(each_y_true)
-        all_y_pred.append(each_y_pred)
-        all_hypno_true.append(each_hypno_true)
-        all_hypno_pred.append(each_hypno_pred)
-
-
-
+    # Duration, loss
     duration = np.round(time.time() - start_time,1)
     total_loss /= n_batches
-    total_y_pred = np.hstack(y)
-    total_y_true = np.hstack(y_true)
 
-    return duration, acc, mf1, k, wf1, f1_w, f1_n1, f1_n2, f1_n3, f1_r, ece, acs, all_y_true, all_y_pred, all_hypno_true, all_hypno_pred
+    # Create save dict
+    save_dict = {
+      "y_true" : all_y_true,
+      "y_pred":all_y_pred,
+      "hypno_true":all_hypno_true,
+      "hypno_pred":all_hypno_pred
+    } 
+
+    return duration, perf_dict, save_dict
 
 
 def predict_on_feature_net(
@@ -244,22 +233,24 @@ def predict_on_feature_net(
             fold_idx=fold_idx
         )
 
+        # Path to restore the trained model
         checkpoint_path = f"{model_dir}/fold{fold_idx}/sleepnetlite/checkpoint/"
+        # Path to load test files
         test_path = f"{model_dir}/fold{fold_idx}/sleepnetlite/data_file{fold_idx}.npz"
 
-        print(f"Checkpoint_path: {checkpoint_path} \n")  
-                  
-        print(f"Test_path: {test_path} \n")
+        #print(f"Checkpoint_path: {checkpoint_path} \n")  
+        #print(f"Test_path: {test_path} \n")
 
+        # Check if checkpoint_path exist
         if not os.path.exists(checkpoint_path):
             Acc.append('NaN')
             
         # Restore the trained model
         saver = tf.train.Saver()
         saver.restore(sess, tf.train.latest_checkpoint(checkpoint_path))
-        print(f"Model restored from: {tf.train.latest_checkpoint(checkpoint_path)}\n")
+        #print(f"Model restored from: {tf.train.latest_checkpoint(checkpoint_path)}\n")
 
-        # Load testing data -
+        # Load testing data 
         x, y, y_smoothed, test_files = data_loader.load_testdata_cv(test_path)
 
         print(f"Patients predicted: {test_files} \n")
@@ -268,7 +259,7 @@ def predict_on_feature_net(
         print(f"[{datetime.now()}] Predicting ",end="",flush=True)
 
         # Evaluate the model on the subject data
-        duration, acc, mf1, k, wf1, f1_w, f1_n1, f1_n2, f1_n3, f1_r, ece, acs, all_y_true, all_y_pred, all_hypno_true, all_hypno_pred = \
+        duration, perf_dict, save_dict = \
             run_epoch(
                 sess=sess, network=test_net,
                 inputs=x, targets=y, targets_smoothed=y_smoothed,
@@ -277,45 +268,39 @@ def predict_on_feature_net(
                 train_op=tf.no_op(),
                 fold_idx=fold_idx
             )
-        print(f"Done! [Time elapsed: {duration} s]")
+        print(f". Done! [Time elapsed: {duration} s]")
 
 
     # Overall performance
-    Acc = np.round(np.mean(acc)*100,1)
-    MF1 = np.round(np.mean(mf1)*100,1)
-    K = np.round(np.mean(k)*100,1)
-    WF1 = np.round(np.mean(wf1)*100,1)
-    F1_w = np.round(np.nanmean(f1_w)*100,1)
-    F1_n1 = np.round(np.nanmean(f1_n1)*100,1)
-    F1_n2 = np.round(np.nanmean(f1_n2)*100,1)
-    F1_n3 = np.round(np.nanmean(f1_n3)*100,1)
-    F1_r = np.round(np.nanmean(f1_r)*100,1)
+    Acc = np.round(np.mean(perf_dict["acc"])*100,1)
+    MF1 = np.round(np.mean(perf_dict["mf1"])*100,1)
+    K = np.round(np.mean(perf_dict["k"])*100,1)
+    WF1 = np.round(np.mean(perf_dict["wf1"])*100,1)
+    F1_w = np.round(np.nanmean(perf_dict["f1_w"])*100,1)
+    F1_n1 = np.round(np.nanmean(perf_dict["f1_n1"])*100,1)
+    F1_n2 = np.round(np.nanmean(perf_dict["f1_n2"])*100,1)
+    F1_n3 = np.round(np.nanmean(perf_dict["f1_n3"])*100,1)
+    F1_r = np.round(np.nanmean(perf_dict["f1_r"])*100,1)
     
-    acc_ = np.round(np.mean(acc),3)
+    acc_ = np.round(np.mean(perf_dict["acc"]),3)
     conf = []
-    for k in ece:
+    for k in perf_dict["ece"]:
       conf.append(k['avg_confidence'])
     conf = np.round(np.mean(conf),3)
     ece_ = round(abs(acc_ - conf),3)
-    acs = f"{np.round(np.mean(acs),3)} ± {np.round(np.std(acs),3)}"
+    acs = f"{np.round(np.mean(perf_dict['acs']),3)} ± {np.round(np.std(perf_dict['acs']),3)}"
 
     print("\nOverall Performance Tables: \n")
     print(tabulate([[dataset, f"DSNL {model}", Acc, MF1, WF1, K, F1_w, F1_n1, F1_n2, F1_n3, F1_r]], headers=['Dataset','Model','Accuracy %', 'MF1 %', 'WF1 %','Cohen-k %', 'W %', 'N1 %', 'N2 %','N3 %','REM %'], tablefmt="pretty"))
     print(tabulate([[dataset, f"DSNL {model}", ece_, acc_, conf, acs]], headers=['Dataset','Model','ECE', 'Accuracy', 'Confidence','ACS'],tablefmt="pretty"))
 
-  
-    # Saving Prediction
-    save_dict = {
-      "y_true" : all_y_true,
-      "y_pred":all_y_pred,
-      "hypno_true":all_hypno_true,
-      "hypno_pred":all_hypno_pred
-    }
-
+    # Saving Predictions
     np.savez(f"/content/drive/MyDrive/Experiments/plot_data/DSNL/output_fold{fold_idx}_{dataset}_{model}.npz", **save_dict)
-    print(f"Prediction saved to path /content/drive/MyDrive/Experiments/plot_data/DSNL/output_fold{fold_idx}_{dataset}_{model}.npz")
+    print(f"\nPrediction saved to path /content/drive/MyDrive/Experiments/plot_data/DSNL/output_fold{fold_idx}_{dataset}_{model}.npz")
 
 def main(argv=None):
+  
+    print(f"Architecture: DSNL, Model: {model} \n")
 
     predict_on_feature_net(
         data_dir=data_dir,
