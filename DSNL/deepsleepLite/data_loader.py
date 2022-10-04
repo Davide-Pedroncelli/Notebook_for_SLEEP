00c947e92d3cfe913e2b4f4dcdcea8a0fb236302
Davide-Pedroncelli <84595751+Davide-Pedroncelli@users.noreply.github.com>
2022-10-04 23:16:24 +0200

    Update data_loader.py

--- DSNL/deepsleepLite/data_loader.py
+++ DSNL/deepsleepLite/data_loader.py
@@ -2,28 +2,27 @@ import os
 import numpy as np
 
 class DataLoader(object):
-
+  
     def __init__(self, data_dir, n_folds, fold_idx):
         self.data_dir = data_dir
         self.n_folds = n_folds
         self.fold_idx = fold_idx
 
-    def _load_npz_file(self, npz_file, cond_prob): # cond_prob = empirical distribution
+    def _load_npz_file(self, npz_file, cond_prob): 
         """Load data and labels from a npz file."""
         cdir = os.getcwd()
         os.chdir(self.data_dir)
         with np.load(npz_file, allow_pickle=True) as f:
-            data = f["x"]  # single channel EEG 'C4-A1'
-            labels = f["y"]
-            sampling_rate = int(f["fs"])
+            data = f["x"] # EEG channel  
+            labels = f["y"] # labels
+            sampling_rate = int(f["fs"]) # sampling rate
             if cond_prob:
-                labels_smoothed = f["y_smoothed"]
+                labels_smoothed = f["y_smoothed"] # LSSC distribution
         os.chdir(cdir)
         return data, labels, labels_smoothed, sampling_rate
 
     def load_testdata_cv(self, data_dir):
-        """Load test data and labels in prediction.py from test list."""
-
+        """Load test data and labels in predict.py from test list."""
         f = np.load(data_dir)
         test_files = f["test_files"].tolist()
         data = []
